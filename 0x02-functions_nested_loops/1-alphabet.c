cc43476b42ca23c7686b761fbd9e4a472b877a6d
Keycia22 <keyciaannie@gmail.com>
2022-09-15 00:54:11 +0200

    Putchar Alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_alphabet -prints 
+alphabet in small letters
+*
+* Return: prints 0 when done
+*/
+void print_alphabet (void)
+{
+    char alpha;
+    for (alpha = 'a'; alpha <= 'z'; alpha++)
+    {
+        _putchar(alpha);
+    }
+    _putchar('\n');
+}
